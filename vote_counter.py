49a05fe1a7080123ebf1f7afb2fbc7ed1f20ffc5
Chelan Patton <nalehc@gmail.com>
2017-11-01 20:38:52 -0700

    Complete vote counter program

--- vote_counter.py
+++ vote_counter.py
@@ -1,7 +1,7 @@
 import csv
 total_votes = 0
 candidate_votes = {}
-with open('election_data_2.csv','r') as file:
+with open("election_data_2.csv","r") as file:
     reader = csv.reader(file)
     next(reader, None)
     for vote in reader:
@@ -11,5 +11,19 @@ with open('election_data_2.csv','r') as file:
             candidate_votes[candidate_name] = 1
         else:
             candidate_votes[candidate_name] += 1
-print (total_votes)
-print (candidate_votes)
+with open("vote_tally.csv", "w") as csvfile:
+    filewriter = csv.writer(csvfile, delimiter=",",
+                            quotechar="|")
+    filewriter.writerow(["Election Results"])
+    filewriter.writerow(["-------------------------"])
+    filewriter.writerow(["Total Votes:", total_votes])
+    for key, value in candidate_votes.items():
+        percent_total = round(value/total_votes*100, 2)
+        filewriter.writerow([(key), (str(percent_total) + "%"), (value)])
+    filewriter.writerow(["Winner:", max(candidate_votes, key = candidate_votes.get)])
+
+with open("vote_tally.csv","r") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
+
